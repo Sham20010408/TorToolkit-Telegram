05086e59280f80a9c8921a9b9043ffc9d792c182
Sham20010408 <90213871+Sham20010408@users.noreply.github.com>
2021-12-09 09:05:07 -0800

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -39,7 +39,7 @@ except:
         #DB_URI = "dbname=tortk user=postgres password=sham2001 host=127.0.0.1 port=5432"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
+        DB_URI = "dbname=tortk user=postgres password=sham2001 host=db port=5432"
         
         # MEGA CONFIG
         MEGA_ENABLE = False
